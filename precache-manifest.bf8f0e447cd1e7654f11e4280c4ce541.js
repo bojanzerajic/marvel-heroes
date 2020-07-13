a33a56cf55a2e52339672c5fb2232d227525a8a8
Zeraja <zerajicbojan@gmail.com>
2020-07-13 12:00:52 +0200

    Updates

---
+++ precache-manifest.bf8f0e447cd1e7654f11e4280c4ce541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da9d06a3415c47d493dbc9c6fd4b182",
+    "url": "/marvel-heroes/index.html"
+  },
+  {
+    "revision": "913462bd662dd6672084",
+    "url": "/marvel-heroes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "36de54a31af40341a87b",
+    "url": "/marvel-heroes/static/css/main.37a37055.chunk.css"
+  },
+  {
+    "revision": "913462bd662dd6672084",
+    "url": "/marvel-heroes/static/js/2.d2735530.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/marvel-heroes/static/js/2.d2735530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36de54a31af40341a87b",
+    "url": "/marvel-heroes/static/js/main.c1273708.chunk.js"
+  },
+  {
+    "revision": "7826c1f5f1eaf95ebe2e",
+    "url": "/marvel-heroes/static/js/runtime-main.b48286e9.js"
+  }
+]);
