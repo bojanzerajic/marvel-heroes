59424ff7011e91c40833a78bcec1cc5aab413a03
Zeraja <zerajicbojan@gmail.com>
2020-07-13 12:54:39 +0200

    Hosting Marvel heroes with smaller updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,8 @@ export const Header = () => {
   return (
     <div className="Header__wrapper col-lg-12 col-md-12 col-sm-12 col-12">
       <div className="row">
-        <Link style="width:725px" to='/'><img
-          className="col-lg-3 col-md-4 col-sm-6 col-12 offset-lg-6"
+        <Link to='/'><img
+          className="linkStyle col-lg-3 col-md-4 col-sm-6 col-12 offset-lg-6"
           src="https://1000logos.net/wp-content/uploads/2017/08/Marvel-emblem.jpg"
           alt="logo"
         />
